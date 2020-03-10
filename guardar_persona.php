0074087d188791bad5f81100233ea7c9590bd05c
Franklin-Molina <asdfglxl7@gmail.com>
2020-03-10 09:55:39 -0500

    Actualizacion

--- guardar_persona.php
+++ guardar_persona.php
@@ -1,55 +1,26 @@
 <?php
 
-//datos conexion
-$host = "localhost";
-$user ="root";
-$password= "";
-$db="datos_php";
-
-
-//Crear conexion
-
-$con =new mysqli($host,$user,$password,$db);
-
-//Prueba de conexion
-
-if ($con->connect_error)
-{
-    echo "error de conexion con la bd";
-
-}else{
-    echo"!!Conexion exitosa con la bd¡¡ <br>";
-}
+include('includes/db.php');
 
 $nombre = $_POST["nombre"];
 $email = $_POST["email"];
 
- if($nombre=="" || $email=="")
- {
-   echo ("Por favor ingrese datos");
- }else{
-/*
-echo "La informacion enviada es: <br> Nombre :$nombre <br> Email : $email";
+  if($nombre=="" || $email==""){
 
-*/
+  }else{
 
-$query ="insert into personas(nombre,email) values('$nombre','$email')";
+    $sql ="insert into personas(nombre,email) values('$nombre','$email')";
+    
+    if (DB::query($sql)){
+        echo "!!Persona guardada correctamente¡¡";
+        }else{
+            echo "Error al guardar la persona".$con->error;
+        }
+      
 
-if ($con->query($query)){
-    echo "!!Persona guardada correctamente¡¡";
-    }else{
-        echo "Error al guardar la persona".$con->error;
-    }
- }
+  }
 
-
-
-
-
-$con->close();
-
-header('Location: crear.php');
-
+  header('Location: crear.php');
 
 ?>
 
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-
-    $conexion= new mysqli('localhost','root','','datos_php');
-  //  $con =new mysqli($host,$user,$password,$db);
+     include('includes/db.php');
+   
+ 
 
 ?>
 
@@ -12,30 +12,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>index</title>
-    <link rel="stylesheet" href="css/styles_index.css"> 
+    <link rel="stylesheet" href="css/styles_index.css">  
     <link rel="icon"href="ico/Favicon.ico">
 </head>
 <body>
     <div class="Tabls">
 
+            <h1> Datos de la Base de datos </h1>
 
-  
-<h1> Datos de la Base de datos </h1>
-
-<table >
-    <tr>
-        <thead>
-         <th>id</th>
-         <th>Nombre</th>
-         <th>Email</th>
-        </thead>
-       
-    </tr>
+      <table >
+         <tr>
+            <thead>
+              <th>id</th>
+              <th>Nombre</th>
+              <th>Email</th>
+           </thead>      
+        </tr>
 
     <?php
 
         $sql="SELECT * FROM personas";
-        $result= mysqli_query($conexion,$sql);
+        $result= DB::query($sql);
+     //  $result= mysqli_query($con,$sql);
 
         while($mostrar= mysqli_fetch_array($result)){
           ?>
@@ -51,15 +49,16 @@
 
 
 
-    ?>
+     ?>
   
-</table>
+      </table>
 
-  <a href="guardar_persona.php"><img src="img/bt3.png" width="150px" height="150%"  alt="logo"></a>
+      <a href="guardar_persona.php" ><img src="img/bt3.png" width="100em" height="100em"  alt="logo"></a>
+      
+  
 
 
 </div>
-      
-    
+  
 </body>
 </html>
